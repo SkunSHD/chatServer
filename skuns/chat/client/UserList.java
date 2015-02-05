af78eb40663582d30cd32f2c3a11e559be9196e8
SKUNS <skunshd@gmail.com>
2015-02-05 12:52:13 +0200

    Initial commit

---
+++ skuns/chat/client/UserList.java
@@ -0,0 +1,34 @@
+package skuns.chat.client;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+import skuns.chat.client.Client;
+
+public class UserList {
+
+	// Храним подключенных пользователей
+	Map<String, Client> onlineUsers = new HashMap<String, Client>();
+
+	public void addUser (String login, Client client) {
+
+		this.onlineUsers.put(login, client);
+		System.out.println(login + " connected");
+	}
+
+	public void deleteUser(String user) {
+		onlineUsers.remove(user);
+	}
+
+	public String[] getUsersInString () {
+		return onlineUsers.entrySet().toArray(new String[0]);
+	}
+
+	public ArrayList<Client> getClientList () {
+		ArrayList<Client> clientList = new ArrayList<Client>();
+		for (Map.Entry<String, Client> m : this.onlineUsers.entrySet()) {
+			clientList.add(m.getValue());
+		}
+		return clientList;
+	}
+}
